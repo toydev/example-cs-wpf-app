7fedc1189b12d2b91c940c41d9f02b2424a2033e
toydev <toy@ownway.info>
2019-08-11 15:21:24 +0900

    各種フレームワーク／ライブラリのバージョンを上げた。

--- ExampleApp/App/App.xaml.cs
+++ ExampleApp/App/App.xaml.cs
@@ -1,17 +1,59 @@
-﻿using System.Windows;
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+using Prism.Ioc;
+using Prism.Mvvm;
+using Prism.Unity;
+using Gu.Localization;
+using Unity;
+
+using ExampleApp.UI;
 
 namespace ExampleApp.App
 {
     /// <summary>
     /// App.xaml の相互作用ロジック
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
-        private void Application_Startup(object sender, StartupEventArgs e)
+        protected override void ConfigureViewModelLocator()
         {
-            var resources = typeof(Properties.Resources);
-            var bootstrapper = new Bootstrapper();
-            bootstrapper.Run();
+            base.ConfigureViewModelLocator();
+
+            ViewModelLocationProvider.SetDefaultViewTypeToViewModelTypeResolver((viewType) =>
+            {
+                var viewName = viewType.FullName;
+                var viewAssemblyName = viewType.Assembly.FullName;
+                var viewModelName = $"{viewName}ViewModel, {viewAssemblyName}";
+                return Type.GetType(viewModelName);
+            });
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        protected override Window CreateShell()
+        {
+            try
+            {
+                Translator.Cultures.Clear();
+                Translator.Cultures.Add(CultureInfo.GetCultureInfo("ja"));
+                Translator.Cultures.Add(CultureInfo.GetCultureInfo("ja-JP"));
+                Translator.Cultures.Add(CultureInfo.GetCultureInfo("en"));
+                Translator.Cultures.Add(CultureInfo.GetCultureInfo("sw"));
+            }
+            catch (Exception)
+            {
+            }
+
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell(Window shell)
+        {
+            shell.Show();
         }
     }
 }
--- Plugin1/Module.cs
+++ Plugin1/Module.cs
@@ -1,10 +1,15 @@
-﻿using Prism.Modularity;
+﻿using Prism.Ioc;
+using Prism.Modularity;
 
 namespace Plugin1
 {
     public class Module : IModule
     {
-        public void Initialize()
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
         {
         }
     }
